3f6534b389646b7cd16259594419048d284dd2c6
Ruben <rub429@gmail.com>
2023-04-09 00:07:07 -0400

    lab

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(length) {
+  let result
+  if (length <= 400) {
+    result = 'This one is on me!'
+  }
+  else if (length > 400 && length < 2000) {
+    result = 'That will be twenty bucks.'
+  }
+  else if (length > 2000 && length < 2500) {
+    result = 'I will gladly take your thirty bucks.'
+  }
+  else {
+    result = 'No can do.'
+  }
+  return result
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(NYC) {
+  let result
+  if (NYC) { 
+    result = 'Ok, sounds good.' 
+  }
+  else { 
+    result = 'No go.' 
+  }
+  return result
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(generous) {
+  let result
+  if (generous) {
+    result = 'Thank you so much.'
+  }
+  else if (not) {
+    result = 'Thank you.'
+  }
+  else {
+    result = 'Bye.'
+  }
+  return result
 }
